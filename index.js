a3fec79646a75622b84224ad85aba6fd2e645644
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-02-02 20:03:39 +0100

    Repetitive lines are combined!

--- index.js
+++ index.js
@@ -17,72 +17,33 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+ /*.then(() => {
     console.log(data[0].title);
     return Recipe.create(data[0]);
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
-
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
-  })
+  })*/
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    for (let i = 0; i < data.length; i++) {
+      for (let i = 0; i < data.length; i++) {
       console.log(data[i].title)
     };
     return Recipe.insertMany(data);
   })
-  .catch((error) => {
-    console.error('Error connecting to the database', error);
-  });
-
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
-  })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
     console.log("Success!");
     return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
   })
-  .catch((error) => {
-    console.error('Error connecting to the database', error);
-  });
-
-  mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
-  })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
     console.log("Success for deleting a recipe!");
     return Recipe.deleteOne({ title: "Carrot Cake" })
   })
-  .catch((error) => {
+  /*.then(() => {
+    console.log("Connection is closed!");
+    mongoose.connection.close()
+  })*/
+  .catch(error => {
     console.error('Error connecting to the database', error);
   });
 
-  mongoose.connection.close();
-
-
-  
-  
-
-
+  mongoose.connection.close()
 
 
 
