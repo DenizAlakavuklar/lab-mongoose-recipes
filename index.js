8b5dbef0ea42d710190d5de931b843d2076d18bd
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-02-02 16:28:04 +0100

    Iteration 3 is completed.

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ mongoose
     console.error('Error connecting to the database', error);
   });
 
+  mongoose
+  .connect(MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+    // Before adding any recipes to the database, let's remove all existing ones
+    return Recipe.deleteMany();
+  })
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+   for (let i=0; i<data.length; i++) {
+    console.log (data[i].title)
+  };
+    return Recipe.insertMany(data);
+    })
+  .catch(error => {
+    console.error('Error connecting to the database', error);
+  });
 
 
 
